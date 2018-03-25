e7b107a34a99eb69922c4c2ef81e44300471e30a
unknown <eduardvoiculescu95@gmail.com>
2018-03-24 21:04:52 -0400

    update

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/eduardvoiculescu/Documents/GitHub/tp2_os
+# Install script for directory: /cygdrive/c/Users/Eddy/Documents/GitHub/tp2_os
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/c/Users/Eddy/Documents/GitHub/tp2_os/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
