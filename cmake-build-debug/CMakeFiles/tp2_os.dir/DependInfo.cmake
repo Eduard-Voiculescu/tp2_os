f7357af766c905aaaf189ebc0d682adf8b1246a2
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2018-03-24 21:01:07 -0400

    mettre tout les fichiers

---
+++ cmake-build-debug/CMakeFiles/tp2_os.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/client/client_thread.c" "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/cmake-build-debug/CMakeFiles/tp2_os.dir/client/client_thread.c.o"
+  "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/client/main.c" "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/cmake-build-debug/CMakeFiles/tp2_os.dir/client/main.c.o"
+  "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/server/main.c" "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/cmake-build-debug/CMakeFiles/tp2_os.dir/server/main.c.o"
+  "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/server/server_thread.c" "/Users/eduardvoiculescu/Documents/GitHub/tp2_os/cmake-build-debug/CMakeFiles/tp2_os.dir/server/server_thread.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../client"
+  "../server"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/tp2_os.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp2_os.dir/client/client_thread.c.o"
+  "CMakeFiles/tp2_os.dir/client/main.c.o"
+  "CMakeFiles/tp2_os.dir/server/main.c.o"
+  "CMakeFiles/tp2_os.dir/server/server_thread.c.o"
+  "tp2_os.pdb"
+  "tp2_os"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tp2_os.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
