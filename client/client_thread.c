808b5170ee9946a15bfcaa61f53ec0b16c1f89a5
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2018-04-01 23:11:34 -0400

    pdate client

--- client/client_thread.c
+++ client/client_thread.c
@@ -82,7 +82,8 @@ ct_code (void *param)
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port_number);
-    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    // arrenger ICI
+    addr.sin_addr.s_addr = htonl("localhost");
     // Connect client socket to server.
     if(connect(client_socket, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
         perror("Erreur lors de la connection.");
